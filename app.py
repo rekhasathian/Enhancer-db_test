20146da45b4ad053e40d7cf5749e847ef68d5275
rekhasathian <98986875+rekhasathian@users.noreply.github.com>
2025-10-23 11:15:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -139,8 +139,8 @@ if page == "📊 Browse Data":
                 key=f"variant_class_{st.session_state.filter_key}"
             )
 
-            st.markdown(""
-            **Additional filters**"")
+            st.markdown("
+            **Additional filters**")
             
             selected_chrom = st.selectbox(
                 "Chromosome",
