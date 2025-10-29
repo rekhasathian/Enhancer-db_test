ef5c5a2ee01f00b5e3e3cc4145ce464cb94e5e02
rekhasathian <98986875+rekhasathian@users.noreply.github.com>
2025-10-29 15:48:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -504,9 +504,9 @@ if page == "📊 Browse Data":
                 st.markdown("### 🧬 Reported Clinical Significance")
 
                 reported_raw = rowd.get("reported_clinical_significance", None)
-                reported = str(reported_raw).strip().lower() if reported_raw is not None and not pd.isna(reported_raw) else "no"
+                # reported = str(reported_raw).strip().lower() if reported_raw is not None and not pd.isna(reported_raw) else "no"
 
-                if reported == "yes":
+                if reported_raw == "yes":
                     st.markdown("**This variant has reported clinical significance.**")
 
                     links = []
