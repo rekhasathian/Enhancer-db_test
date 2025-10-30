9dacabbb70e334b06068528fd98f40b939e60072
rekhasathian <98986875+rekhasathian@users.noreply.github.com>
2025-10-30 13:05:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ if page == "📊 Browse Data":
                     ).any(axis=1)
                 ]
 
-                if filtered_df.empty:
+                if filtered_display_df.empty:
                     st.warning(f"No results found for **'{search_query}'**. Please try a different keyword.")
                 else:
                     filtered_display_df = filtered_display_df[display_cols].drop_duplicates()
